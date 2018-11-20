eb5c28e8fff16eef7005ac7801f950278b61951f
XshreyasguptaX <shreyas.charming@gmail.com>
2018-11-20 09:26:56 +0530

    added scripts for installtion

---
+++ Brewfile
@@ -0,0 +1,46 @@
+#taps
+tap 'caskroom/cask'
+tap 'caskroom/eid'
+tap 'caskroom/fonts'
+tap 'caskroom/versions'
+tap 'homebrew/bundle'
+
+#brew package installation
+brew gcc@8
+brew 'gdb'
+brew 'python'
+brew 'python3'
+brew 'thefuck'
+brew 'zsh'
+brew 'zsh-completions'
+brew 'wget'
+brew 'git'
+brew 'git-lfs'
+brew 'gnuplot'
+brew 'htop'
+brew 'tree'
+brew 'mas'
+brew 'git'
+
+#cask installations
+cask 'xquartz'
+cask 'android-platform-tools'
+cask 'google-chrome'
+cask 'visual-studio-code'
+cask 'vlc'
+cask 'java'
+
+#Mac App store apps
+mas 'Hacker News YC' id: 1039600682
+mas 'The Unarchiver' id: 425424353 
+mas 'Evernote' id: 406056744 
+mas 'Houseparty' id: 1381523962 
+mas 'Adblock Plus' id: 1432731683 
+mas 'Betternet VPN' id: 1028905953 
+mas 'Slack' id: 803453959 
+mas 'WhatsApp' id: 1147396723 
+mas 'Bear' id: 1091189122 
+mas 'Spark' id: 1176895641 
+mas 'Amphetamine' id: 937984704 
+mas 'Transmit' id: 1436522307 
+mas 'Shazam' id: 897118787 
---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Make ZSH the default shell environment
+chsh -s $(which zsh)
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
+#first, specify where you want all your applications to be installed
+cask_args appdir: '/Applications'
+
+# Set macOS preferences
+# We will run this last because this will reload the shell
+source .macos
